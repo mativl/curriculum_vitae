64258a5cadc86914b0aaf85b20f5b65eadcf2ca6
mvallarino <matias.vallarino@gmail.com>
2019-08-22 17:02:17 -0500

    Agrega circle progress

---
+++ src/components/CircularProgress.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-progress-circular
+    :rotate="-90"
+    :size="100"
+    :width="15"
+    :value="init"
+    color="primary"
+  >{{ init }}</v-progress-circular>
+</template>
+
+<script>
+export default {
+  props:{
+    value: Number
+  },
+  data () {
+    return {
+      interval: {},
+      init: 0,
+    }
+  },
+  beforeDestroy () {
+    clearInterval(this.interval)
+  },
+  mounted () {
+    this.interval = setInterval(() => {
+      this.init += 1;
+      if (this.init >= this.value) {
+        clearInterval(this.interval)
+      }
+    }, 10);
+  }
+}
+</script>
+
+<style>
+  .v-progress-circular {
+    margin: 1rem;
+  }
+</style>
--- src/components/Container.vue
+++ src/components/Container.vue
@@ -10,6 +10,11 @@
       <v-flex mb-12 xs12>
         <Timeline></Timeline>
       </v-flex>
+      <v-flex mb-12 xs12>
+        <CircularProgress :value="45"></CircularProgress>
+        <CircularProgress :value="75"></CircularProgress>
+        <CircularProgress :value="25"></CircularProgress>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -17,11 +22,13 @@
 <script>
 import Card from './Card';
 import Timeline from './Timeline';
+import CircularProgress from './CircularProgress';
 
 export default {
   components: {
     Card,
     Timeline,
+    CircularProgress,
   }
 };
 </script>
